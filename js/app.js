7fa2deeae2350384a639a88e04f78442c6762160
Peter StJ <regardingscot@gmail.com>
2013-03-18 13:47:49 +0200

    Clean up

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@ goog.provide('app');
 goog.require('tl.control.Timeline');
 
 /**
- * Entry point for the app.
+ * @fileoverview  Entry point for the application, this is merely a wrapper
+ *   around the functionality.
+ *
+ * @author regardingscot@gmail.com (Peter StJ)
  */
 app = function() {
-
-  //If we want to test - load stubs instead
-  // Load stubs into the resource provider
-  //tl.loader.loadFakeData(goog.global['STUBS']);
+  // If we want to test - load stubs instead (assuming those are already
+  // provided in the global scope).
+  // tl.loader.loadFakeData(goog.global['STUBS']);
 
   // At the end this should be enough to run the whole application.
   (new tl.control.Timeline());
-
 };
 
 app();
